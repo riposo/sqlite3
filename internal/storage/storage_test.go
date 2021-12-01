44545c7b0d02deb9bd9cad8c265928f1627278d2
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2021-12-01 15:17:09 +0000

    Prepare for v0.2.0 (#2)

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -11,8 +11,8 @@ import (
 	"github.com/riposo/riposo/pkg/params"
 	"github.com/riposo/riposo/pkg/riposo"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	. "github.com/bsm/ginkgo"
+	. "github.com/bsm/gomega"
 	. "github.com/riposo/sqlite3/internal/storage"
 )
 
